6e08bb31e9411fe6623cc12876b2740f017b3a97
Tejal02-mali <tejal02115@gmail.com>
2026-03-15 14:03:56 +0530

    Update test_gemini.py

--- backend/test_gemini.py
+++ backend/test_gemini.py
@@ -1,14 +1,18 @@
+from fastapi import FastAPI
 from google import genai
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
+app = FastAPI()
+
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-response = client.models.generate_content(
-  model="gemini-2.0-flash",
-    contents="Explain logistic regression in simple terms."
-)
-
-print(response.text)
+@app.get("/")
+def home():
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents="Explain logistic regression in simple terms."
+    )
+    return {"response": response.text}
